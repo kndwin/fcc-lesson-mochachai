2073bd371a456342e7f185c081249ae86c5e8e6d
kndwin <tnkevino@gmail.com>
2020-07-06 19:11:23 +1000

    Fixed the Comparision test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -91,7 +91,7 @@ suite('Unit Tests', function(){
     /** 9 - .isBelow() => a < b , .isAtLeast =>  a >= b **/
     test('#isBelow, #isAtLeast', function() {
       assert.isAtLeast('world'.length , 5);
-      assert.isAbove(2*Math.random(), 0);
+      assert.isAtLeast(2*Math.random(), 0);
       assert.isBelow(5 % 2, 2);
       assert.isBelow(2/3, 1);
     });
